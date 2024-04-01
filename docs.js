5a8a6f53b9f26980cedead5ed635fae1746a5f85
KyuuRzy <160400183+KyuuRzy@users.noreply.github.com>
2024-04-01 17:30:31 +0800

    Create docs.js

---
+++ docs.js
@@ -0,0 +1,12 @@
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('docs.json');
+const express = require('express');
+const sh = express.Router();
+
+sh.use(express.json());
+sh.use(express.urlencoded({
+	extended: true
+}));
+
+sh.use('/playground', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+module.exports = sh;
